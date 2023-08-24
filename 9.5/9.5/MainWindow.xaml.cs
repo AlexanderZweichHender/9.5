1ffef46cc9827c3ee6b3cebe1defc70fb909afec
AlexanderZweichHender <93994885+AlexanderZweichHender@users.noreply.github.com>
2023-08-24 10:32:05 -0700

    Task1

---
+++ 9.5/9.5/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace _9._5
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOnClicked(object sender, RoutedEventArgs e) 
+        {
+            var text = textBox.Text;
+            if (!string.IsNullOrEmpty(text))
+            {
+                var words = text.Split(' ');
+
+                foreach (var word in words)
+                    listBox.Items.Add(word);
+            }
+            else
+            {
+                MessageBox.Show("text is empty");
+            }
+        }
+    }
+}
