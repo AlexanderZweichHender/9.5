3637c7945b8b1e188be260b69144d3185fb49ae9
AlexanderZweichHender <93994885+AlexanderZweichHender@users.noreply.github.com>
2023-08-25 07:24:04 -0700

    task2 complete

--- 9.5/9.5/MainWindow.xaml.cs
+++ 9.5/9.5/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Linq;
+using System.Windows;
 
 namespace _9._5
 {
@@ -7,6 +9,38 @@ namespace _9._5
         public MainWindow()
         {
             InitializeComponent();
-        }        
+        }
+        
+        private void OnSplitButtonClicked(object sender, EventArgs e)
+        {
+            var text = inputTextBox.Text;
+            if (!string.IsNullOrEmpty(text))
+            {
+                var words = text.Split(' ');
+                foreach (var word in words)
+                    outputListBox.Items.Add(word);
+            }
+            else
+            {
+                MessageBox.Show("Textbox is empty");
+            }
+        }
+
+        private void ReverseButtonOnClicked(Object sender, EventArgs e) 
+        {
+            var text = textBox.Text;
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                var words = text.Split(' ');
+                Array.Reverse(words);
+
+                outputLabel.Content = string.Join(" ", words);
+            }
+            else
+            {
+                MessageBox.Show("TextBox is empty");
+            }
+        }
     }
 }
